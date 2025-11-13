a679bd437aed2a9a4914382651c016be79d5e255
Alex Bolduc <axbolduc@gmail.com>
2025-11-13 12:26:44 -0500

    remove wallet from sidebar

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -35,12 +35,6 @@
 			name: 'Collections',
 			route: '/app/collections',
 			icon: 'M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z'
-		},
-		{
-			name: 'Wallet',
-			route: '/app/wallet',
-			icon: 'M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z',
-			divider: true
 		}
 	];
 </script>
